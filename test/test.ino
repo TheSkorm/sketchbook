ef15e6eb24fd78b29d1435fd29d4e3dc6b5f52ca
Michael Wheeler <mwheeler@ardmore.erin.qld.au.fooproject.net>
2012-05-22 20:40:26 +1000

    Inserted the setup code

--- test/test.ino
+++ test/test.ino
@@ -33,6 +33,8 @@ void setup() {
 	debug("SERIAL",1,"STARTED"); 
 	setup_temp(6);
   setup_relays();
+  setup_sdcard();
+  setup_network();
 }
 
 byte read_dht11_dat(int DHT11_PIN)
@@ -127,7 +129,7 @@ void loop()
     debug("HUMID",7,String(String(latesttemp.humid) + "." + String(latesttemp.humid_point)));
     debug("TEMP",7,String(String(latesttemp.temp) + "." + String(latesttemp.temp_point)));
 
-    Serial.println(relay_toggle(33));
+    //Serial.println(relay_toggle(33));
 
   }
   delay(20) ;       
@@ -146,6 +148,8 @@ void debug(String component, int subcomponent, String message){
 		Serial.print(subcomponent);
 		Serial.print(" - ");
 		Serial.println(message);
+    Serial.print("MEM/0 - Free:");
+    Serial.println(freeMemory());
 	}
 }
 
@@ -175,3 +179,28 @@ void setup_relays(){
   pinMode(35, OUTPUT);
   debug("RELAYS",-1,"STARTED");
 }
+
+void setup_sdcard(){
+ // initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
+  // breadboards.  use SPI_FULL_SPEED for better performance.
+  pinMode(10, OUTPUT);                       // set the SS pin as an output (necessary!)
+  digitalWrite(10, HIGH);                    // but turn off the W5100 chip!
+
+  if (!card.init(SPI_HALF_SPEED, 4)) debug("SD",10,"CARD INIT FAILED");
+  
+  // initialize a FAT volume
+  if (!volume.init(&card)) debug("SD",10,"VOLUME INIT FAILED");
+
+  if (!root.openRoot(&volume)) debug("SD",10,"OPEN ROOT FAILED");
+  
+
+}
+
+void setup_network(){
+
+  Ethernet.begin(mac);
+  W5100.setRetransmissionTime(0x07D0); // This code is meant to stoy lock ups with wiznet
+  W5100.setRetransmissionCount(3); 
+  server.begin();
+
+  }
