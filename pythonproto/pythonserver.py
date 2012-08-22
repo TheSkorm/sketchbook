3a3eb1f94af12b88813a244708cf7e6f92970ec5
Michael Wheeler <michael@michael-wheeler.org>
2012-08-22 17:42:37 +1000

    threads bitch!

--- pythonproto/pythonserver.py
+++ pythonproto/pythonserver.py
@@ -1,94 +1,125 @@
 import socket
 import struct
-
-
+import BaseHTTPServer
+import SocketServer
+import threading
 
 TCP_IP = '59.167.158.119'
 TCP_PORT = 58008
-BUFFER_SIZE=7         #start,action,address,value,value2,end
-
-#MESSAGE = "\xff\x05\x02\x02\x02\xA5\x00"
-#MESSAGE2 = "\xff\x03\x02\x00\x01\xA5\x00"
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.settimeout(2)
-s.connect((TCP_IP, TCP_PORT))
-
-#s.send(MESSAGE)
-#s.send(MESSAGE2)
-
-def send_keepalive():
-    message = "\xff\x00\x00\x00\x00\xA5\x00"
-    s.send(message)
-
-def send_digitalread(port):
-    message = "\xff\x01"+struct.pack("!B",port)+"\x00\x00\xA5\x00"
-    s.send(message)
-
-def send_analogread(port):
-    message = "\xff\x02"+struct.pack("!B",port)+"\x00\x00\xA5\x00"
-    s.send(message)
-
-def send_digitalwrite(port,state):
-    message = "\xff\x03"+struct.pack("!B",port)+"\x00"+struct.pack("!B",state)+"\xA5\x00"
-    s.send(message)
-
-def send_setoutput(port,state):
-    message = "\xff\x05"+struct.pack("!B",port)+"\x00"+struct.pack("!B",state)+"\xA5\x00"
-    s.send(message)
+BUFFER_SIZE=7   
 
 
-def recv_data():
-    data = s.recv(BUFFER_SIZE)
-    output = {}
-    if (len(data)==7):
-        output["start"] = struct.unpack("!B",data[0])[0]
-        output["action"] = struct.unpack("!B",data[1])[0]
-        output["address"] = struct.unpack("!B",data[2])[0]
-        output["value"] = struct.unpack("!B",data[3])[0]
-        output["value2"] = struct.unpack("!B",data[4])[0]
-        output["checksum"] = struct.unpack("!B",data[5])[0]
-    return output
 
-def action_data(packet):
-    #TODO ADD CHECKS
-    actions = {
-0: recv_loopback,
-1: recv_read_digital,
-2: recv_read_analog,
-3: recv_write_digital,
-4: recv_read_dht,
-5: recv_set_output
-}
-    actions[packet["action"]](packet)
-    
+class http_handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("<html><head></head><body>test</body></html>")
+        elif self.path == "/test":
+            global a
+            a.send_keepalive()
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("<html><head></head><body>Sent Keep Alive</body></html>")
+        else:
+            self.send_error(404, "File not found")
 
 
-def recv_loopback(packet):
-    print "Got loopback"
-
-def recv_read_digital(packet):
-    print "D" + str(packet["address"]) + " - " + str(packet["value2"])
-    return
 
 
-def recv_read_analog(packet):
-    return
 
-def recv_write_digital(packet):
-    return
 
-def recv_read_dht(packet):
-    return
 
-def recv_set_output(packet):
-    return
 
-send_keepalive()
-send_setoutput(3,2)
-send_digitalread(2)
-send_setoutput(3,0)
-send_digitalwrite(3,1)
+class ArduinoControl(threading.Thread):
+    def run(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((TCP_IP, TCP_PORT))
+        self.send_keepalive()
+        while 1:
+            self.action_data(self.recv_data())
+    def send_keepalive(self):
+        message = "\xff\x00\x00\x00\x00\xA5\x00"
+        self.s.send(message)
 
-while (True):
-    action_data(recv_data())
+    def send_digitalread(self,port):
+        message = "\xff\x01"+struct.pack("!B",port)+"\x00\x00\xA5\x00"
+        self.s.send(message)
+
+    def send_analogread(self,port):
+        message = "\xff\x02"+struct.pack("!B",port)+"\x00\x00\xA5\x00"
+        self.s.send(message)
+
+    def send_digitalwrite(self,port,state):
+        message = "\xff\x03"+struct.pack("!B",port)+"\x00"+struct.pack("!B",state)+"\xA5\x00"
+        self.s.send(message)
+
+    def send_setoutput(self,port,state):
+        message = "\xff\x05"+struct.pack("!B",port)+"\x00"+struct.pack("!B",state)+"\xA5\x00"
+        self.s.send(message)
+
+
+    def recv_data(self):
+        data = self.s.recv(BUFFER_SIZE)
+        output = {}
+        if (len(data)==7):
+            output["start"] = struct.unpack("!B",data[0])[0]
+            output["action"] = struct.unpack("!B",data[1])[0]
+            output["address"] = struct.unpack("!B",data[2])[0]
+            output["value"] = struct.unpack("!B",data[3])[0]
+            output["value2"] = struct.unpack("!B",data[4])[0]
+            output["checksum"] = struct.unpack("!B",data[5])[0]
+        return output
+
+    def action_data(self,packet):
+        #TODO ADD CHECKS
+        actions = {
+    0: self.recv_loopback,
+    1: self.recv_read_digital,
+    2: self.recv_read_analog,
+    3: self.recv_write_digital,
+    4: self.recv_read_dht,
+    5: self.recv_set_output
+    }
+        actions[packet["action"]](packet)
+        
+
+
+    def recv_loopback(self,packet):
+        print "Got loopback"
+
+    def recv_read_digital(self,packet):
+        print "D" + str(packet["address"]) + " - " + str(packet["value2"])
+        return
+
+
+    def recv_read_analog(self,packet):
+        return
+
+    def recv_write_digital(self,packet):
+        return
+
+    def recv_read_dht(self,packet):
+        return
+
+    def recv_set_output(self,packet):
+        return
+
+##send_keepalive()
+##send_setoutput(3,2)
+##send_digitalread(2)
+##send_setoutput(3,0)
+##send_digitalwrite(3,1)
+
+##while (True):
+##    action_data(recv_data())
+
+a = ArduinoControl()
+a.start()
+
+server_address = ('', 8088)
+httpd = BaseHTTPServer.HTTPServer(server_address, http_handler)
+httpd.serve_forever()
